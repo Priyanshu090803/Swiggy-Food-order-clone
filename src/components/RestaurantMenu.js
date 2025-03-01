a3cd39c3978885fd00c78f3f52550099a1eb28c9
Priyanshu090803 <priyanshu122a@gmail.com>
2025-03-01 22:52:30 +0530

    1March

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react";
 
 const RestaurantMenu =()=>{
-    const [resMenu,setResMenu]= useState();
+    const [resMenu,setResMenu]= useState(null);
     useEffect(()=>{
       fetchMenu();
     },[])
     let fetchMenu = async()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.87960&lng=78.07620&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
         const json= await data.json();
-        console.log(json)
+        // console.log(json)
+    console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        
         setResMenu(json.data);
+        // setResMenu(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
+
+    // const {name,avgStarRating}=resMenu?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.info;
+    const { name = "No name available", avgStarRating = "No rating available" } =
+  resMenu?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.info || {};
 
 
     return(
         <div>
-            <h1>{}</h1>
+                <h1> {name}</h1>
+                <h4>{avgStarRating}</h4>
+
         </div>
     )
 }
