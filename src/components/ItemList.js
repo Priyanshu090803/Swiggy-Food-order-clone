0e7021bcfe31c221556aeade70d6e296a049c5cc
Priyanshu090803 <priyanshu122a@gmail.com>
2025-03-31 23:24:34 +0530

    31 march

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,11 +4,23 @@ const ItemList =({items})=>{
         <div>
             {
                 items?.map((item)=>(
-                    <div key={item?.card?.info?.id || Math.random()}>
-                    <div>
+                    <div key={item?.card?.info?.id} className=" p-2 m-2 relative border-gray-200 border-b-2">
+                    <div className=" p-2">
                         <span>{item?.card?.info?.name}</span>
+                        
+                        <span key={item?.card?.info?.id}> 
+                        
+                         -₹{item?.card?.info?.price ? 
+                        (item?.card?.info?.price/100):
+                        (item?.card?.info?.defaultprice/100)}
+                        
+                        </span>
                     </div>
+                    <p className=" text-xs ml-2 text-rose-400">{item?.card?.info?.description}</p>
+
+                 <button className=" w-16 h-8 top-8 rounded-lg cursor-pointer active:text-amber-200 right-4 absolute bg-black text-white">+Add</button>
                     </div>
+
                 ))
             }
         </div>
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,13 +2,19 @@ import React from 'react'
 import ItemList from './ItemList'
 const RestaurantCategory = ({data}) => {
   return (
-    <div className=' w-6/12 mx-80 my-10 h-20 shadow-2xl rounded-lg shadow-red-200 bg-blue-50  m-5 relative pt-2' >
+    <div>
+    <div className=' w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4' >
+    <div className=' flex justify-between'>
+    
     <span className=' h-5 ml-2 font-bold text-lg'>{data.title}({data.itemCards.length})</span>
-    <span className=' absolute right-5'>⬇️</span>
-      <ItemList items ={data.itemCards}/>
+    <span className=' cursor-pointer'>⬇️</span>
+
+    </div>
+    
+    <ItemList items ={data.itemCards}/>
      
      </div>
-     
+     </div>
   )
 }
 
