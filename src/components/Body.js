d8d15e65a1a6bacfc5f3b48cf7f36a46af84296a
Priyanshu090803 <priyanshu122a@gmail.com>
2025-04-20 18:45:54 +0530

    fix: fixed cors issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,7 +14,7 @@ const Body = () => {
     const fetchData= async()=>{
     try{
 
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.87960&lng=78.07620&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch("https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.87960&lng=78.07620&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
     console.log(json)
     setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [])
